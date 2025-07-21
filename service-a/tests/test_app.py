a83d9c0e4b8f9a0f6cb6572452e418e3651d104f
Daniel Pahima <danielp@aero-nous.com>
2025-07-21 13:09:20 +0300

    test13

--- service-a/tests/test_app.py
+++ service-a/tests/test_app.py
@@ -15,5 +15,6 @@ def test_hello(client):
     assert response.status_code == 200
     
 
+
     # בודק שהתגובה מכילה את הטקסט "Hello, World!"
     assert response.data.decode('utf-8') == "Hello, World!"
